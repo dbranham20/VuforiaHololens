90e8852cb8562f951ddaa2093168397506f631d1
Daniel Branham <idanielbranham@gmail.com>
2018-06-07 16:07:33 -0400

    Initial commit

---
+++ Assets/ARCameraPos.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARCameraPos : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log.
+        Debug.Log("AR camera POSITION: " + transform.position);
+        Debug.Log("AR camera ROTATION: " + transform.rotation);
+		
+	}
+}
